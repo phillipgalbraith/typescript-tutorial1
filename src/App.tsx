dd1cbf45d85b4ae8e13b01669efc3ab29b15af40
Phil G <fillupgalbraith@gmail.com>
2022-08-02 21:17:16 -0700

    Added styles and started building TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import InputFeild from './components/InputFeild';
+import TodoList from './components/TodoList';
+import { Todo } from './model';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), isDone: false }]);
+      setTodo("");
+    }
+  };
+
   return <div className = "App">
     <span className="heading">Taskify</span>
+    <InputFeild handleAdd={handleAdd} todo={todo} setTodo={setTodo}/>
+    <TodoList todos={todos} setTodos={setTodos}/>
   </div>;
 }
 
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Todo } from '../model.ts';
+
+interface Props {
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({ todos, setTodos }: Props) => {
+  return (
+    <div className="todos">
+        {
+        }
+    TodoList</div>
+  )
+}
+
+export default TodoList
